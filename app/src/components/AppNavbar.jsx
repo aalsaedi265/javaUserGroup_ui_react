5c92579ea0fb6a7e9fec3fb1ac8261bf5840dbd8
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-06-24 12:31:02 -0400

    navbar

--- app/src/components/AppNavbar.jsx
+++ app/src/components/AppNavbar.jsx
@@ -1,13 +1,31 @@
 
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import '../css/AppNavbar.css';
 
 function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-                Navbar
-            </a>
-        </nav>
+          <div className="navbar">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <button className="navbar-toggler" onClick={() => setIsOpen(!isOpen)}>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={`navbar-collapse ${isOpen ? 'show' : ''}`}>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://www.linkedin.com/in/ahmed-r-j-alsaedi/">Linkedin</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="https://github.com/aalsaedi265">Github</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
     );
 }
 
