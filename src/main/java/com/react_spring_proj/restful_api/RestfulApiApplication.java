c6ce66c4b3bdf2c0e2e48edf26ca7e4747a7ee74
Ahmed Alsaedi <aalsaedi265@gmail.com>
2024-07-03 14:59:32 -0400

    auth0, env secret, auth0cli

--- src/main/java/com/react_spring_proj/restful_api/RestfulApiApplication.java
+++ src/main/java/com/react_spring_proj/restful_api/RestfulApiApplication.java
@@ -3,10 +3,14 @@ package com.react_spring_proj.restful_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RestfulApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
 		SpringApplication.run(RestfulApiApplication.class, args);
 	}
 
